b4ccc2e42df5495be2127a701d07cdeea2fd87ee
Web3Servicios <134114048+Web3Servicios@users.noreply.github.com>
2023-05-30 23:46:09 -0400

    Add files via upload

---
+++ kubo-master/core/commands/dag/import.go
@@ -0,0 +1,163 @@
+package dagcmd
+
+import (
+	"errors"
+	"io"
+
+	"github.com/ipfs/boxo/coreiface/options"
+	"github.com/ipfs/boxo/files"
+	gocarv2 "github.com/ipfs/boxo/ipld/car/v2"
+	cid "github.com/ipfs/go-cid"
+	cmds "github.com/ipfs/go-ipfs-cmds"
+	ipld "github.com/ipfs/go-ipld-format"
+	ipldlegacy "github.com/ipfs/go-ipld-legacy"
+
+	"github.com/ipfs/kubo/core/commands/cmdenv"
+	"github.com/ipfs/kubo/core/commands/cmdutils"
+)
+
+func dagImport(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+	node, err := cmdenv.GetNode(env)
+	if err != nil {
+		return err
+	}
+
+	api, err := cmdenv.GetApi(env, req)
+	if err != nil {
+		return err
+	}
+
+	// on import ensure we do not reach out to the network for any reason
+	// if a pin based on what is imported + what is in the blockstore
+	// isn't possible: tough luck
+	api, err = api.WithOptions(options.Api.Offline(true))
+	if err != nil {
+		return err
+	}
+
+	doPinRoots, _ := req.Options[pinRootsOptionName].(bool)
+
+	// grab a pinlock ( which doubles as a GC lock ) so that regardless of the
+	// size of the streamed-in cars nothing will disappear on us before we had
+	// a chance to roots that may show up at the very end
+	// This is especially important for use cases like dagger:
+	//    ipfs dag import $( ... | ipfs-dagger --stdout=carfifos )
+	//
+	if doPinRoots {
+		unlocker := node.Blockstore.PinLock(req.Context)
+		defer unlocker.Unlock(req.Context)
+	}
+
+	// this is *not* a transaction
+	// it is simply a way to relieve pressure on the blockstore
+	// similar to pinner.Pin/pinner.Flush
+	batch := ipld.NewBatch(req.Context, api.Dag())
+
+	roots := cid.NewSet()
+	var blockCount, blockBytesCount uint64
+
+	it := req.Files.Entries()
+	for it.Next() {
+		file := files.FileFromEntry(it)
+		if file == nil {
+			return errors.New("expected a file handle")
+		}
+
+		// import blocks
+		err = func() error {
+			// wrap a defer-closer-scope
+			//
+			// every single file in it() is already open before we start
+			// just close here sooner rather than later for neatness
+			// and to surface potential errors writing on closed fifos
+			// this won't/can't help with not running out of handles
+			defer file.Close()
+
+			car, err := gocarv2.NewBlockReader(file)
+			if err != nil {
+				return err
+			}
+
+			for _, c := range car.Roots {
+				roots.Add(c)
+			}
+
+			for {
+				block, err := car.Next()
+				if err != nil && err != io.EOF {
+					return err
+				} else if block == nil {
+					break
+				}
+				if err := cmdutils.CheckBlockSize(req, uint64(len(block.RawData()))); err != nil {
+					return err
+				}
+
+				// the double-decode is suboptimal, but we need it for batching
+				nd, err := ipldlegacy.DecodeNode(req.Context, block)
+				if err != nil {
+					return err
+				}
+
+				if err := batch.Add(req.Context, nd); err != nil {
+					return err
+				}
+				blockCount++
+				blockBytesCount += uint64(len(block.RawData()))
+			}
+			return nil
+		}()
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := batch.Commit(); err != nil {
+		return err
+	}
+
+	// It is not guaranteed that a root in a header is actually present in the same ( or any )
+	// .car file. This is the case in version 1, and ideally in further versions too.
+	// Accumulate any root CID seen in a header, and supplement its actual node if/when encountered
+	// We will attempt a pin *only* at the end in case all car files were well-formed.
+
+	// opportunistic pinning: try whatever sticks
+	if doPinRoots {
+		err = roots.ForEach(func(c cid.Cid) error {
+			ret := RootMeta{Cid: c}
+
+			// This will trigger a full read of the DAG in the pinner, to make sure we have all blocks.
+			// Ideally we would do colloring of the pinning state while importing the blocks
+			// and ensure the gray bucket is empty at the end (or use the network to download missing blocks).
+			if block, err := node.Blockstore.Get(req.Context, c); err != nil {
+				ret.PinErrorMsg = err.Error()
+			} else if nd, err := ipldlegacy.DecodeNode(req.Context, block); err != nil {
+				ret.PinErrorMsg = err.Error()
+			} else if err := node.Pinning.Pin(req.Context, nd, true); err != nil {
+				ret.PinErrorMsg = err.Error()
+			} else if err := node.Pinning.Flush(req.Context); err != nil {
+				ret.PinErrorMsg = err.Error()
+			}
+
+			return res.Emit(&CarImportOutput{Root: &ret})
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	stats, _ := req.Options[statsOptionName].(bool)
+	if stats {
+		err = res.Emit(&CarImportOutput{
+			Stats: &CarImportStats{
+				BlockCount:      blockCount,
+				BlockBytesCount: blockBytesCount,
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ kubo-master/core/commands/name/name.go
@@ -0,0 +1,270 @@
+package name
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/ipfs/boxo/ipns"
+	ipns_pb "github.com/ipfs/boxo/ipns/pb"
+	cmds "github.com/ipfs/go-ipfs-cmds"
+	cmdenv "github.com/ipfs/kubo/core/commands/cmdenv"
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagcbor"
+	"github.com/ipld/go-ipld-prime/codec/dagjson"
+	"github.com/libp2p/go-libp2p/core/peer"
+	mbase "github.com/multiformats/go-multibase"
+)
+
+type IpnsEntry struct {
+	Name  string
+	Value string
+}
+
+var NameCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline: "Publish and resolve IPNS names.",
+		ShortDescription: `
+IPNS is a PKI namespace, where names are the hashes of public keys, and
+the private key enables publishing new (signed) values. In both publish
+and resolve, the default name used is the node's own PeerID,
+which is the hash of its public key.
+`,
+		LongDescription: `
+IPNS is a PKI namespace, where names are the hashes of public keys, and
+the private key enables publishing new (signed) values. In both publish
+and resolve, the default name used is the node's own PeerID,
+which is the hash of its public key.
+
+You can use the 'ipfs key' commands to list and generate more names and their
+respective keys.
+
+Examples:
+
+Publish an <ipfs-path> with your default name:
+
+  > ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+  Published to QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+
+Publish an <ipfs-path> with another name, added by an 'ipfs key' command:
+
+  > ipfs key gen --type=rsa --size=2048 mykey
+  > ipfs name publish --key=mykey /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+  Published to QmSrPmbaUKA3ZodhzPWZnpFgcPMFWF4QsxXbkWfEptTBJd: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+
+Resolve the value of your name:
+
+  > ipfs name resolve
+  /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+
+Resolve the value of another name:
+
+  > ipfs name resolve QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ
+  /ipfs/QmSiTko9JZyabH56y2fussEt1A5oDqsFXB3CkvAqraFryz
+
+Resolve the value of a dnslink:
+
+  > ipfs name resolve ipfs.io
+  /ipfs/QmaBvfZooxWkrv7D3r8LS9moNjzD2o525XMZze69hhoxf5
+
+`,
+	},
+
+	Subcommands: map[string]*cmds.Command{
+		"publish": PublishCmd,
+		"resolve": IpnsCmd,
+		"pubsub":  IpnsPubsubCmd,
+		"inspect": IpnsInspectCmd,
+	},
+}
+
+type IpnsInspectValidation struct {
+	Valid     bool
+	Reason    string
+	PublicKey peer.ID
+}
+
+// IpnsInspectEntry contains the deserialized values from an IPNS Entry:
+// https://github.com/ipfs/specs/blob/main/ipns/IPNS.md#record-serialization-format
+type IpnsInspectEntry struct {
+	Value        string
+	ValidityType *ipns_pb.IpnsEntry_ValidityType
+	Validity     *time.Time
+	Sequence     uint64
+	TTL          *uint64
+	PublicKey    string
+	SignatureV1  string
+	SignatureV2  string
+	Data         interface{}
+}
+
+type IpnsInspectResult struct {
+	Entry      IpnsInspectEntry
+	Validation *IpnsInspectValidation
+}
+
+var IpnsInspectCmd = &cmds.Command{
+	Status: cmds.Experimental,
+	Helptext: cmds.HelpText{
+		Tagline: "Inspects an IPNS Record",
+		ShortDescription: `
+Prints values inside of IPNS Record protobuf and its DAG-CBOR Data field.
+Passing --verify will verify signature against provided public key.
+`,
+		LongDescription: `
+Prints values inside of IPNS Record protobuf and its DAG-CBOR Data field.
+
+The input can be a file or STDIN, the output can be JSON:
+
+  $ ipfs routing get "/ipns/$PEERID" > ipns_record
+  $ ipfs name inspect --enc=json < ipns_record
+
+Values in PublicKey, SignatureV1 and SignatureV2 fields are raw bytes encoded
+in Multibase. The Data field is DAG-CBOR represented as DAG-JSON.
+
+Passing --verify will verify signature against provided public key.
+
+`,
+	},
+	Arguments: []cmds.Argument{
+		cmds.FileArg("record", true, false, "The IPNS record payload to be verified.").EnableStdin(),
+	},
+	Options: []cmds.Option{
+		cmds.StringOption("verify", "CID of the public IPNS key to validate against."),
+	},
+	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+		file, err := cmdenv.GetFileArg(req.Files.Entries())
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		var b bytes.Buffer
+
+		_, err = io.Copy(&b, file)
+		if err != nil {
+			return err
+		}
+
+		var entry ipns_pb.IpnsEntry
+		err = proto.Unmarshal(b.Bytes(), &entry)
+		if err != nil {
+			return err
+		}
+
+		encoder, err := mbase.EncoderByName("base64")
+		if err != nil {
+			return err
+		}
+
+		result := &IpnsInspectResult{
+			Entry: IpnsInspectEntry{
+				Value:        string(entry.Value),
+				ValidityType: entry.ValidityType,
+				Sequence:     *entry.Sequence,
+				TTL:          entry.Ttl,
+				PublicKey:    encoder.Encode(entry.PubKey),
+				SignatureV1:  encoder.Encode(entry.SignatureV1),
+				SignatureV2:  encoder.Encode(entry.SignatureV2),
+				Data:         nil,
+			},
+		}
+
+		if len(entry.Data) != 0 {
+			// This is hacky. The variable node (datamodel.Node) doesn't directly marshal
+			// to JSON. Therefore, we need to first decode from DAG-CBOR, then encode in
+			// DAG-JSON and finally unmarshal it from JSON. Since DAG-JSON is a subset
+			// of JSON, that should work. Then, we can store the final value in the
+			// result.Entry.Data for further inspection.
+			node, err := ipld.Decode(entry.Data, dagcbor.Decode)
+			if err != nil {
+				return err
+			}
+
+			var buf bytes.Buffer
+			err = dagjson.Encode(node, &buf)
+			if err != nil {
+				return err
+			}
+
+			err = json.Unmarshal(buf.Bytes(), &result.Entry.Data)
+			if err != nil {
+				return err
+			}
+		}
+
+		validity, err := ipns.GetEOL(&entry)
+		if err == nil {
+			result.Entry.Validity = &validity
+		}
+
+		verify, ok := req.Options["verify"].(string)
+		if ok {
+			key := strings.TrimPrefix(verify, "/ipns/")
+			id, err := peer.Decode(key)
+			if err != nil {
+				return err
+			}
+
+			result.Validation = &IpnsInspectValidation{
+				PublicKey: id,
+			}
+
+			err = ipns.ValidateWithPeerID(id, &entry)
+			if err == nil {
+				result.Validation.Valid = true
+			} else {
+				result.Validation.Reason = err.Error()
+			}
+		}
+
+		return cmds.EmitOnce(res, result)
+	},
+	Type: IpnsInspectResult{},
+	Encoders: cmds.EncoderMap{
+		cmds.Text: cmds.MakeTypedEncoder(func(req *cmds.Request, w io.Writer, out *IpnsInspectResult) error {
+			tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', 0)
+			defer tw.Flush()
+
+			fmt.Fprintf(tw, "Value:\t%q\n", string(out.Entry.Value))
+			fmt.Fprintf(tw, "Validity Type:\t%q\n", out.Entry.ValidityType)
+			if out.Entry.Validity != nil {
+				fmt.Fprintf(tw, "Validity:\t%s\n", out.Entry.Validity.Format(time.RFC3339Nano))
+			}
+			fmt.Fprintf(tw, "Sequence:\t%d\n", out.Entry.Sequence)
+			if out.Entry.TTL != nil {
+				fmt.Fprintf(tw, "TTL:\t%d\n", *out.Entry.TTL)
+			}
+			fmt.Fprintf(tw, "PublicKey:\t%q\n", out.Entry.PublicKey)
+			fmt.Fprintf(tw, "Signature V1:\t%q\n", out.Entry.SignatureV1)
+			fmt.Fprintf(tw, "Signature V2:\t%q\n", out.Entry.SignatureV2)
+
+			data, err := json.Marshal(out.Entry.Data)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(tw, "Data:\t%s\n", string(data))
+
+			if out.Validation == nil {
+				tw.Flush()
+				fmt.Fprintf(w, "\nThis record was not validated.\n")
+			} else {
+				tw.Flush()
+				fmt.Fprintf(w, "\nValidation results:\n")
+
+				fmt.Fprintf(tw, "\tValid:\t%v\n", out.Validation.Valid)
+				if out.Validation.Reason != "" {
+					fmt.Fprintf(tw, "\tReason:\t%s\n", out.Validation.Reason)
+				}
+				fmt.Fprintf(tw, "\tPublicKey:\t%s\n", out.Validation.PublicKey)
+			}
+
+			return nil
+		}),
+	},
+}
